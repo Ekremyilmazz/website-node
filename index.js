a8766fb9a8626751707d0b5d8c37a35efdef990e
Ekremyilmazz <ekremyilmaz4217@gmail.com>
2024-08-31 23:09:43 +0300

    express.js dersleri

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-var http = require("http");
-var fs =require("fs");
+const express = require("express");
+const app=express();
 
-var server =http.createServer((req, res) =>{
-    
-     if(req.url == "/"){
-        fs.readFile("index.html", (err, html)=> {
-            res.write(html);
-            res.end();
-        }) 
-     }else if(req.url == "/products"){
-        fs.readFile("urunler.html", (err, html)=> {
-            res.write(html);
-            res.end();
-        }) 
-     }else{
-        fs.readFile("404.html", (err, html)=> {
-            res.write(html);
-            res.end();
-        }) 
-     }
-     
+app.set("view engine","ejs");
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+
+const data=[
+    {id: 1, name:"iPhone 14", price:30000, isActive: true, imageUrl:"1.jpeg"},
+    {id: 2, name:"iPhone 15", price:40000, isActive: false, imageUrl:"1.jpeg"},
+    {id: 3, name:"iPhone 16", price:50000, isActive: true, imageUrl:"1.jpeg"}
+];
+
+//routes
+app.use("/products/:id", function(req, res){
+    const urun=data.find(u =>u.id == req.params.id);
+    res.render("product-details", urun);
 });
 
-server.listen(4000, () =>{
-    console.log("Node.js server at port 4000");
+app.use("/products", function(req, res){
+    res.render("products", {
+        urunler :data
+    });
+});
+
+app.use("/", function(req, res){
+    res.render("index");
+});
+
+app.listen(3000, ()=>{
+    console.log("Listening on port 3000");
 });
